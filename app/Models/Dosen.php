586f7a6f93943460670dbf9b07980bdf1ecbb592
vaniilaa <rizna275@gmail.com>
2024-04-01 13:25:18 +0700

    Tugas Praktikum

--- app/Models/Dosen.php
+++ app/Models/Dosen.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Dosen extends Model
 {
     use HasFactory;
+    protected $table = 'dosen';
 }
--- resources/views/dosen.blade.php
+++ resources/views/dosen.blade.php
@@ -1,5 +1,35 @@
 @extends('layouts.master')
 
 @section('container')
-    
+<h1 class="text-center">Data Dosen</h1>
+<div class="mb-4">
+<button type="button" class="btn btn-success ">Tambah Data</button>
+</div>
+<div class="row">
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nama</th>
+            <th scope="col">NIDN</th>
+            <th scope="col">Mata Kuliah</th>
+            <th scope="col">Aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($dosen as $dsn)
+          <tr>
+            <th scope="row">{{$dsn->id_dosen}}</th>
+            <td>{{$dsn->nama}}</td>
+            <td>{{$dsn->nidn}}</td>
+            <td>{{$dsn->matkul}}</td>
+            <td>
+                <button type="button" class="btn btn-primary">Update</button>
+                <button type="button" class="btn btn-danger">Hapus</button>
+            </td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+</div>
 @endsection
