438cf1b9605ddfb86f0255d540e76dbdee50bc54
vaniilaa <150694455+vaniilaa@users.noreply.github.com>
2024-04-06 15:50:39 +0700

    Tugas Praktikum

--- resources/views/dosen.blade.php
+++ resources/views/dosen.blade.php
@@ -14,7 +14,7 @@
             <th scope="col">Mata Kuliah</th>
             <th scope="col">Fakultas</th>
             <th scope="col">NIDN</th>
-            <th scope="col">Aksi</th>
+            <th scope="col">nom</th>
           </tr>
         </thead>
         <tbody>
